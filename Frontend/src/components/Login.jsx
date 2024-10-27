24fea48c3b9654b1d27648295a3dc938b606a99a
karanacharya <karanacharya0987@gmail.com>
2024-10-27 15:16:00 +0530

    assignment_2 first commit

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   const handleLogin = async (e) => {  
     e.preventDefault();
     try {
-      const response = await axios.post('/api/auth/v1/login', {
+      const response = await axios.post('http://localhost:7000/api/auth/v1/login', {
         username,
         password
       });
